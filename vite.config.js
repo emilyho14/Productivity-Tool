68bd4f9225878bd002572a44df8f4787f05ca068
emilyho14 <eeh2003@sbcglobal.net>
2025-10-18 01:19:31 -0500

    deployment attempt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace <repo-name> with your repo
 export default defineConfig({
   plugins: [react()],
+  base: '/ADHDRewardSystem/', 
 })
