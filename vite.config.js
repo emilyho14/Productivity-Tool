4b9c0e1b174a9525934f3f356d3c96028e3cd71e
emilyho14 <eeh2003@sbcglobal.net>
2025-11-06 01:26:23 -0600

    attempting endpoint change again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({mode}) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/Productivity-Tool' : '/',
+  base: '/Productivity-Tool',
+  // base: mode === 'production' ? '/Productivity-Tool' : '/',
 }))
